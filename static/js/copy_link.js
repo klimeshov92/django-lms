b1901cdff2fd2bce83c54dc6d19ba11ccf47c06c
klimeshov92 <117681025+klimeshov92@users.noreply.github.com>
2024-06-16 21:13:13 +0300

    много всего

--- static/js/copy_link.js
+++ static/js/copy_link.js
@@ -4,13 +4,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Проверяем, существует ли элемент
     if (copyLinkElement) {
+        console.log("Элемент с id 'copy_link' найден");
+
         // Если элемент существует, добавляем к нему обработчик события клика
         copyLinkElement.addEventListener("click", function(event) {
             event.preventDefault(); // Отменяем стандартное действие перехода по ссылке
 
-            // Создаем временное текстовое поле и устанавливаем его значение равным URL из атрибута href ссылки
+            console.log("Клик по элементу с id 'copy_link' произведен");
+
+            // Получаем URL из атрибута href ссылки
+            var urlToCopy = event.target.getAttribute("href");
+            console.log("URL для копирования:", urlToCopy);
+
+            // Создаем временное текстовое поле и устанавливаем его значение равным URL
             var urlField = document.createElement("input");
-            urlField.value = event.target.getAttribute("href");
+            urlField.value = urlToCopy;
 
             // Добавляем временное текстовое поле в тело документа
             document.body.appendChild(urlField);
@@ -19,14 +27,19 @@ document.addEventListener("DOMContentLoaded", function() {
             urlField.select();
 
             // Копируем выделенный текст в буфер обмена
-            document.execCommand("copy");
+            var successful = document.execCommand("copy");
+            if (successful) {
+                console.log("Текст успешно скопирован в буфер обмена");
+                alert("Ссылка скопирована в буфер обмена");
+            } else {
+                console.log("Не удалось скопировать текст в буфер обмена");
+                alert("Не удалось скопировать ссылку в буфер обмена");
+            }
 
             // Удаляем временное текстовое поле из документа
             document.body.removeChild(urlField);
-
-            // Выводим сообщение об успешном копировании
-            alert("Ссылка скопирована в буфер обмена");
         });
+    } else {
+        console.log("Элемент с id 'copy_link' не найден");
     }
-    // Если элемент не найден, ничего не делаем.
 });
